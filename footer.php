67050747296980ffd4a869982ab5d75be6fd23f4
ShejulShubham <148786777+ShejulShubham@users.noreply.github.com>
2026-02-05 14:48:40 +0530

    added rest of the root files

---
+++ footer.php
@@ -0,0 +1,10 @@
+  <!-- ------------JQuery CDN------- -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- _________________slider js_______________ -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- ------------Ecommerce Js------- -->
+    <script src="https://s3-ap-southeast-1.amazonaws.com/assets.ngauge.co/javascripts/ecommerce.js"></script>
+    <script src="assets/js/all.js" defer></script>
+</body>
+
+</html>
